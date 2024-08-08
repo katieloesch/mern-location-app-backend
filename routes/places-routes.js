eff8ec2db07da0473b2d7ff5ac25e65a93ba05ee
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-08 01:03:43 +0100

    add GET route to get place by id

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,82 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('get request in places');
+const DUMMY_PLACES = [
+  {
+    id: 'p0',
+    title: 'Camden Head',
+    description: 'best hunting ground for reindeer enthusiasts',
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh0g9I_ZYibJtTuldCxDJBGB0r7-TiU2EeA&s',
+    address: '100 Camden High St, London NW1 0LU',
+    coordinates: {
+      lat: 51.536388,
+      lng: -0.140556,
+    },
+    creator: 'u0',
+  },
+  {
+    id: 'p1',
+    title: 'Camden Head',
+    description: 'best hunting ground for reindeer enthusiasts',
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh0g9I_ZYibJtTuldCxDJBGB0r7-TiU2EeA&s',
+    address: '100 Camden High St, London NW1 0LU',
+    coordinates: {
+      lat: 51.536388,
+      lng: -0.140556,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imgUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u0',
+  },
+  {
+    id: 'p3',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imgUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u1',
+  },
+];
 
-  res.json({ msg: 'get route working' });
+const DUMMY_USERS = [
+  {
+    id: 'u0',
+    name: 'Sara',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRePiA58k0fr1ctJENADPDrYvsENw6hmaPK5g&s',
+    places: 7,
+  },
+  {
+    id: 'u1',
+    name: 'John',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6ecNMnwcSGzb36MWQ_o96_y-J1xKEJrWDjw&s',
+    places: 3,
+  },
+];
+
+router.get('/:placeId', (req, res, next) => {
+  const placeId = req.params.placeId;
+
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  res.json({ place });
 });
 
 module.exports = router;
