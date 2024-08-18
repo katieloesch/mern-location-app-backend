00ffdb0a2446a665be19a1972d3dac47338cfe3f
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-18 05:06:08 +0100

    make api call to get corrdinates for an address

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ### Frontend Repository
 
 https://github.com/katieloesch/mern-location-app-frontend
+
+<!--
+
+18/08/2024
+
+add validation to users controller and places controller using express-validator package
+
+
+ -->
---
+++ util/location.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const API_KEY = process.env.MAPS_API_KEY;
+const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+async function getCoordsForAddress(address) {
+  //   return {
+  //     lat: 40.7484474,
+  //     lng: -73.9871516,
+  //   };
+
+  const response = await axios.get(
+    `${API_URL}${encodeURIComponent(address)}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find location for the specified address.',
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
