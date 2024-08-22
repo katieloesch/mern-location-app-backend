44d33a736c6d2ef13979518de6c3fab8c4420efb
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-22 03:53:40 +0100

    update login function in users-controller to connect to mongoose instead of dummy array

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -71,17 +71,27 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: newUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = DUMMY_USERS.find((user) => user.email === email);
+
+  let user;
+
+  try {
+    user = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError('Log in failed, please try again later.', 500);
+    return next(error);
+  }
 
   if (!user || user.password !== password) {
-    throw new HttpError(
-      'Could not identify user, credentials seem to be wrong.',
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
       401
     );
+    return next(error);
   }
-  res.json({ msg: 'logged in!' });
+
+  res.json({ msg: 'Logged in!' });
 };
 
 module.exports = {
