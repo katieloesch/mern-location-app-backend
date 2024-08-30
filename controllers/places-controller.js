9a7cfa2bee05e4a5fbdffb87638654c8cf56833f
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-30 07:25:46 +0100

    delete images when places get deleted

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 
@@ -187,6 +188,8 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = place.image;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -204,6 +207,10 @@ const deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+
+  fs.unlink(imagePath, (err) => {
+    console.log(err);
+  });
 
   res.status(200).json({ msg: 'Deleted place.' });
 };
